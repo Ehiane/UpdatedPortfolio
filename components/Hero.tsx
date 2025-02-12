bdbb88510fe4fd921f86d6a229267dbda07ab408
Ehiane <e.oigiagbe@wsu.edu>
2025-02-12 12:16:57 -0800

    Implemented Spotlight to Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+// 'rafce' shortcut for react hero template (only accessible with es7+ extension installed)
+
+const Hero = () => {
+    return (
+        <div className='pb-20 pt36'>
+            <div>
+                <Spotlight />
+            </div>
+        </div>
+    )
+}
+
+export default Hero
