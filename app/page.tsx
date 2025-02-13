0104afac6847eb2a83c72516e8b3fe119facb82f
Ehiane <e.oigiagbe@wsu.edu>
2025-02-12 16:21:08 -0800

    Added data items and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavBar";
-import {FaHome } from "react-icons/fa"
+import {FaHome } from "react-icons/fa";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -11,6 +13,7 @@ export default function Home() {
               {name: 'Home', link: '/', icon: <FaHome /> }
           ]}/>
         <Hero />
+        <Grid />
       </div>
     </main>
   );
